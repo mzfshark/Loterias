41ac62a3602c36f0e4134ec716a6d8c1adb970ab
Mauricio Zanetti Filho <contact@thinkincoin.com>
2024-09-05 16:46:04 -0300

    test

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,12 @@
 require('@nomiclabs/hardhat-ethers');
 require('dotenv').config();
 
+const { DEPLOYER_PRIVATE_KEY } = process.env;
+
+if (!DEPLOYER_PRIVATE_KEY || DEPLOYER_PRIVATE_KEY.length !== 64) {
+  throw new Error("Invalid DEPLOYER_PRIVATE_KEY: Ensure it is set and 64 characters long.");
+}
+
 module.exports = {
   solidity: "0.8.7",
   networks: {
@@ -13,23 +19,23 @@ module.exports = {
     harmony: {
       url: `https://api.harmony.one`,
       chainId: 1666600000,
-      accounts: [`${secrets.DEPLOYER_PRIVATE_KEY}`] 
+      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`] 
     },
     bsc: {
       url: `https://bsc-mainnet.infura.io/v3/554262fab79f49adb4fdba2db2587800`,
-      accounts: [`${secrets.DEPLOYER_PRIVATE_KEY}`]
+      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`]
     },
     bsc_testnet: {
       url: `https://bsc-testnet.infura.io/v3/554262fab79f49adb4fdba2db2587800`,
-      accounts: [`${secrets.DEPLOYER_PRIVATE_KEY}`]
+      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`]
     },
     opBNB: {
       url: `https://opbnb-mainnet.infura.io/v3/554262fab79f49adb4fdba2db2587800`,
-      accounts: [`${secrets.DEPLOYER_PRIVATE_KEY}`]
+      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`]
     },
     opBNB_testnet: {
       url: `https://opbnb-testnet.infura.io/v3/554262fab79f49adb4fdba2db2587800`,
-      accounts: [`${secrets.DEPLOYER_PRIVATE_KEY}`]
+      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`]
     },
   },
 };
