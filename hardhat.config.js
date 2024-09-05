14131c4fbff09e117752282c26640a8577c6986d
Mauricio Zanetti Filho <contact@thinkincoin.com>
2024-09-05 16:05:20 -0300

    add networks harmony

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,5 +9,20 @@ module.exports = {
     //   url: `https://ropsten.infura.io/v3/YOUR_INFURA_PROJECT_ID`,
     //   accounts: [`0x${YOUR_PRIVATE_KEY}`]
     // }
+    harmony: {
+      url: `https://api.harmony.one`
+    },
+    bsc: {
+      url: `https://bsc-mainnet.infura.io/v3/554262fab79f49adb4fdba2db2587800`
+    },
+    bsc_testnet: {
+      url: `https://bsc-testnet.infura.io/v3/554262fab79f49adb4fdba2db2587800`
+    },
+    opBNB: {
+      url: `https://opbnb-mainnet.infura.io/v3/554262fab79f49adb4fdba2db2587800`
+    },
+    opBNB_testnet: {
+      url: `https://opbnb-testnet.infura.io/v3/554262fab79f49adb4fdba2db2587800`
+    },
   },
 };
