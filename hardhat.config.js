42af4783262fe1c086b2ffc267a2e63a0b6885a8
Mauricio Zanetti Filho <contact@thinkincoin.com>
2024-09-05 16:28:53 -0300

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,19 +17,19 @@ module.exports = {
     },
     bsc: {
       url: `https://bsc-mainnet.infura.io/v3/554262fab79f49adb4fdba2db2587800`,
-      accounts: [process.env.DEPLOYER_PRIVATE_KEY]
+      accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`]
     },
     bsc_testnet: {
       url: `https://bsc-testnet.infura.io/v3/554262fab79f49adb4fdba2db2587800`,
-      accounts: [process.env.DEPLOYER_PRIVATE_KEY]
+      accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`]
     },
     opBNB: {
       url: `https://opbnb-mainnet.infura.io/v3/554262fab79f49adb4fdba2db2587800`,
-      accounts: [process.env.DEPLOYER_PRIVATE_KEY]
+      accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`]
     },
     opBNB_testnet: {
       url: `https://opbnb-testnet.infura.io/v3/554262fab79f49adb4fdba2db2587800`,
-      accounts: [process.env.DEPLOYER_PRIVATE_KEY]
+      accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`]
     },
   },
 };
