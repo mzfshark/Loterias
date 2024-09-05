58097afc2ff1631162ad60ecadbb4671c3c87e34
Mauricio Zanetti Filho <contact@thinkincoin.com>
2024-09-05 14:06:35 -0300

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,13 @@
 require('@nomiclabs/hardhat-ethers');
-require('@nomiclabs/hardhat-etherscan');
-require('dotenv').config();
 
 module.exports = {
   solidity: "0.8.7",
   networks: {
-    mainnet: {
-      url: `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-      accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`]
-    }
+    hardhat: {},
+    // Add other networks here if needed
+    // e.g., ropsten: {
+    //   url: `https://ropsten.infura.io/v3/YOUR_INFURA_PROJECT_ID`,
+    //   accounts: [`0x${YOUR_PRIVATE_KEY}`]
+    // }
   },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY
-  }
 };
