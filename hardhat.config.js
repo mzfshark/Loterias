321ed69d06038812e1f15f6374a34dbde6fe94b2
Mauricio Zanetti Filho <contact@thinkincoin.com>
2024-09-05 17:03:56 -0300

    test

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
 require('@nomiclabs/hardhat-ethers');
 require('dotenv').config();
 
+// Get environment variables
+const { INFURA_PROJECT_ID, INFURA_PROJECT_SECRET, DEPLOYER_PRIVATE_KEY } = process.env;
+
+// Validate environment variables
+if (!DEPLOYER_PRIVATE_KEY || DEPLOYER_PRIVATE_KEY.length !== 64) {
+  throw new Error("Invalid DEPLOYER_PRIVATE_KEY: Ensure it is set and 64 characters long.");
+}
+if (!INFURA_PROJECT_ID) {
+  throw new Error("INFURA_PROJECT_ID is not set in environment variables.");
+}
+if (!INFURA_PROJECT_SECRET) {
+  throw new Error("INFURA_PROJECT_SECRET is not set in environment variables.");
+}
 
 module.exports = {
   solidity: "0.8.7",
@@ -14,23 +27,23 @@ module.exports = {
     harmony: {
       url: `https://api.harmony.one`,
       chainId: 1666600000,
-      accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`] 
+      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`] 
     },
     bsc: {
       url: `https://bsc-mainnet.infura.io/v3/554262fab79f49adb4fdba2db2587800`,
-      accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`]
+      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`]
     },
     bsc_testnet: {
       url: `https://bsc-testnet.infura.io/v3/554262fab79f49adb4fdba2db2587800`,
-      accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`]
+      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`]
     },
     opBNB: {
       url: `https://opbnb-mainnet.infura.io/v3/554262fab79f49adb4fdba2db2587800`,
-      accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`]
+      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`]
     },
     opBNB_testnet: {
       url: `https://opbnb-testnet.infura.io/v3/554262fab79f49adb4fdba2db2587800`,
-      accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`]
+      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`]
     },
   },
 };
